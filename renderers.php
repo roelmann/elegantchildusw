77562e248cc65ae8565b0797ac1340a85bed9b58
roelmann <oelmann.richard@gmail.com>
2014-03-27 11:47:30 +0000

    Add bootstrap style to Turn Editing On button

--- renderers.php
+++ renderers.php
@@ -1,4 +1,31 @@
 <?php
+class theme_elegantchildusw_core_renderer extends theme_elegance_core_renderer {
+/**
+* Returns HTML to display a "Turn editing on/off" button in a form.
+*
+* @param moodle_url $url The URL + params to send through when clicking the button
+* @return string HTML the button
+* Written by G J Bernard
+*/
+
+    public function edit_button(moodle_url $url) {
+        $url->param('sesskey', sesskey());
+        if ($this->page->user_is_editing()) {
+            $url->param('edit', 'off');
+            $btn = 'btn-danger';
+            $title = get_string('turneditingoff');
+            $icon = 'fa fa-minus-square-o';
+        } else {
+            $url->param('edit', 'on');
+            $btn = 'btn-success';
+            $title = get_string('turneditingon');
+            $icon = 'fa fa-edit';
+        }
+        return html_writer::tag('a', html_writer::start_tag('i', array('class' => $icon.' fa-white'))." ".$title.
+               html_writer::end_tag('i'), array('href' => $url, 'class' => 'btn '.$btn, 'title' => $title));
+    }
+}
+
 include_once ($CFG->dirroot."/blocks/navigation/renderer.php");
 
 class theme_elegantchildusw_block_navigation_renderer extends block_navigation_renderer {
